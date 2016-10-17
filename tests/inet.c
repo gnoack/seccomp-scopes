0519e698772b46d77d6e8d5ab56dfdf60a246e6e
Guenther Noack <guenther@unix-ag.uni-kl.de>
2016-10-17 08:32:35 +0200

    Add inet test

---
+++ tests/inet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "testlib.h"
+
+void test_ipv4tcp() { socket(AF_INET, SOCK_STREAM, 0); }
+void test_ipv6tcp() { socket(AF_INET6, SOCK_STREAM, 0); }
+void test_ipv4udp() { socket(AF_INET, SOCK_DGRAM, 0); }
+void test_ipv6udp() { socket(AF_INET6, SOCK_DGRAM, 0); }
+void test_x25()     { socket(AF_X25, SOCK_DGRAM, 0); }
+void test_inetraw() { socket(AF_INET, SOCK_RAW, 0); }
+
+int main(int argc, char* argv[]) {
+  init_test(argc, argv);
+
+  // socket() without inet permission crashes.
+  expect_crash("stdio", test_ipv4tcp);
+
+  // socket() with inet permission in working scenarios.
+  expect_ok("stdio inet", test_ipv4tcp);
+  expect_ok("stdio inet", test_ipv6tcp);
+  expect_ok("stdio inet", test_ipv4udp);
+  expect_ok("stdio inet", test_ipv6udp);
+
+  // socket() with inet permission with x25 crashes
+  expect_crash("stdio inet", test_x25);
+  // socket() with inet permission with raw socket crashes
+  expect_crash("stdio inet", test_inetraw);
+}
