c322f2de91e8f1c86aa897ef3aab91b8c5c4072c
Guenther Noack <guenther@unix-ag.uni-kl.de>
2018-01-03 01:10:11 +0100

    Musl support for gethostbyname()

--- pledge_dns.c
+++ pledge_dns.c
@@ -66,5 +66,8 @@ void append_dns_filter(unsigned int scopes, struct sock_fprog* prog) {
 
     // musl specifics.
     _RET_EQ(__NR_fcntl64, SECCOMP_RET_ALLOW);
+    _RET_EQ(__NR_readv, SECCOMP_RET_ALLOW);
+    _RET_EQ(__NR_socket, SECCOMP_RET_ALLOW);  // TODO: This is too broad.
+    _RET_EQ(__NR_poll, SECCOMP_RET_ALLOW);
   }
 }
